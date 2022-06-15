27616cb6a2670e5429e51dcf2a436b7d8f12c70e
Jessica Turner <jesyika@gmail.com>
2022-06-15 14:47:20 +1000

    Cleaned up code - added arg for input filesizes instead of being hardcoded

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # automate_image_export
+
 Python script that automates saving images at different resolutions in Affinity Designer and moving them into a folder
+
+## Pre-requisites 
+
+- Python 3.7
+- PyAutoGUI
+
+## Setup
+
+1. Install python3
+
+2. Install PyAutoGUI:
+```bash
+    python3 -m pip install pyautogui
+```
+
+## Run
+
+**NOTE:** Has to be run from the folder where Affinity Designer saves the pictures
+```bash
+    python3 automate_export.py "helloworld" --list 128 512
+```
+
--- automate_export.py
+++ automate_export.py
@@ -1,31 +1,81 @@
 # Automate exporting files using tablet as bottom screen - Affinity Designer
 # Has to be run from the folder where Affinity Designer saves the pictures
 
+from ast import parse
 import pyautogui as pag
 import time
 import os
 import argparse
+from dataclasses import dataclass  # Python 3.7
 
-filesizes = ["32", "64", "128", "256", "512", "1024"]
-#filesizes = ["128", "256", "512", "1024", "1280"]
+
+@dataclass
+class Coord:
+    x: int
+    y: int
+
+
+# Coord - (x, y)
+# fileButton - File dropdown menu location
+fileButton = Coord(2993, 24)
+# exportButton - File -> Export...
+exportButton = Coord(3027, 548)
+# sizeField - The first box next to 'Size:' under Export Settings
+sizeField = Coord(4154, 666)
+# exportSettingsButtton - The Export button under Export Settings
+exportSettingsButton = Coord(4553, 960)
+# saveButton -
+saveButton = Coord(3972, 1014)
+
 filetype = ".png"
 
-# Coords - (x, y)
-# fileButton - File dropdown menu location
-# exportButton - File -> Export...
-# sizeField - The first box next to 'Size:' under Export Settings
-# exportSettingsButtton - The Export button under Export Settings
-# saveButton - 
-fileButton = (2993, 24)
-exportButton = (3027, 548)
-sizeField = (4154, 666)
-exportSettingsButton = (4553, 960)
-saveButton = (3972, 1014)
+
+def save_and_move(filename, filesizes, filepath):
+    for i in range(len(filesizes)):
+        # click on File
+        pag.click(fileButton.x, fileButton.y)
+
+        # click on export
+        pag.click(exportButton.x, exportButton.y)
+
+        # click on size
+        pag.click(sizeField.x, sizeField.y)
+
+        # delay to make sure size is entered
+        time.sleep(1.0)
+
+        # enter a size
+        pag.typewrite(filesizes[i])
+
+        # click on export
+        pag.click(exportSettingsButton.x, exportSettingsButton.y)
+
+        # type in save filename
+        savename = "{0}_{1}{2}".format(
+            filename, filesizes[i], filetype)
+        pag.typewrite(savename)
+        print("[{0}/{1}] Saving file: {2} in './'.".format(i +
+              1, len(filesizes), savename))
+
+        # click on save
+        pag.click(saveButton.x, saveButton.y)
+
+        # another delay to make sure the save box is closed - and that the save file exists
+        time.sleep(1.0)
+
+        # should move file to directory
+        os.rename("./{}".format(savename),
+                  "{0}/{1}".format(filepath, savename))
+        print("[{0}/{1}] Moving file from './{2}' to '{3}/{2}'.".format(i +
+              1, len(filesizes), savename, filepath))
 
 
 def main(*args):
     parser = argparse.ArgumentParser()
-    parser.add_argument("FILENAME", metavar="filename", type=str, help="name of the file")
+    parser.add_argument("FILENAME", metavar="filename",
+                        type=str, help="name of the file")
+    parser.add_argument("--list", dest="FILESIZES",
+                        nargs="+", help="--list 1234 5678")
     args = parser.parse_args()
 
     filepath = "./"+args.FILENAME
@@ -39,41 +89,10 @@ def main(*args):
         else:
             print("Successfully created the directory %s " % filepath)
 
-    for i in range(len(filesizes)):
-        # click on File
-        pag.click(fileButton[0], fileButton[1])
-
-        # click on export
-        pag.click(exportButton[0], exportButton[1])
-
-        # click on size
-        pag.click(sizeField[0], sizeField[1])
-
-        # delay to make sure size is entered
-        time.sleep(1.0)
-
-        # enter a size
-        pag.typewrite(filesizes[i])
-
-        # click on export
-        pag.click(exportSettingsButton[0], exportSettingsButton[1])
-
-        # type in save filename
-        savename = "{0}_{1}{2}".format(args.FILENAME, filesizes[i], filetype)
-        pag.typewrite(savename)
-        print("[{0}/{1}] Saving file: {2} in './'.".format(i+1, len(filesizes), savename))
-
-        # click on save
-        pag.click(saveButton[0], saveButton[1])
-
-        # another delay to make sure the save box is closed - and that the save file exists
-        time.sleep(1.0)
-
-        # should move file to directory
-        os.rename("./{}".format(savename), "{0}/{1}".format(filepath, savename))
-        print("[{0}/{1}] Moving file from './{2}' to '{3}/{2}'.".format(i+1, len(filesizes), savename, filepath))
+    save_and_move(args.FILENAME, args.FILESIZES)
 
     print("Program finished!")
+
 
 if __name__ == '__main__':
     main()
